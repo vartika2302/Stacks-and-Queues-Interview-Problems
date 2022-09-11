e61bf7d3705bdc69439741715f09c4fd4e3a07dd
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-09-11 13:27:14 +0530

    Time: 3 ms (43.18%), Space: 7 MB (25.83%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -0,0 +1,56 @@
+class MyStack {
+public:
+    queue<int>q1;
+    queue<int>q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        if(q1.empty()) return -1;
+        while(q1.size()>1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int el = q1.front();
+        q1.pop();
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return el;
+    }
+    
+    int top() {
+        if(q1.empty()) return -1;
+        while(q1.size()>1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int el = q1.front();
+        q2.push(el);
+        q1.pop();
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return el;
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
